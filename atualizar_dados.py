c07f3b372e581a20dd0794eacd8253c690a4cc83
Robert Souza <robert.infnity@gmail.com>
2026-04-22 07:15:43 -0300

    Update atualizar_dados.py

--- atualizar_dados.py
+++ atualizar_dados.py
@@ -8,10 +8,13 @@ urls = {
     "2026_LoL_esports_match_data_from_OraclesElixir.csv": "https://oracleselixir-downloadable-files.s3-us-west-2.amazonaws.com/2026_LoL_esports_match_data_from_OraclesElixir.csv"
 }
 
-# --- A MÁSCARA ---
-# Engana o servidor da Amazon fazendo-o achar que somos um navegador de um usuário real
+# --- A MÁSCARA TURBINADA (Com Crachá de Visitante) ---
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "Accept-Language": "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7",
+    "Referer": "https://oracleselixir.com/", # O "Crachá" que engana a Amazon
+    "Connection": "keep-alive"
 }
 
 def baixar_arquivos():
